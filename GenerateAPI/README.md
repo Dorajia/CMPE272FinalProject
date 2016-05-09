f91c1730cbf3e3bd362a537455896cf04dbf47a0
Dora Jia <iboboy13@gmail.com>
2016-05-09 22:55:44 +0000

    Update the script to get Time from Google API

--- GenerateAPI/README.md
+++ GenerateAPI/README.md
@@ -3,18 +3,27 @@
 ## Installation : 
 
 Download or clone the code.
-
-Run MongoDB on default instance.
-
-Run program by typing ```npm start``` on terminal.
+ Run 
+ 
+ node Server.js
 
 ## Testing REST api : 
 
 Open up REST simulator ( Postman recommended )
+<<<<<<< HEAD
+
+Hit following URL's : 
+Http://localhost:8080/data/37.756746/-122.537572
+X Y are original points
+
+The hexagonGeometry is stored in the collection with name hexagonGeometry
+the HexagonCenter is stored in the collection with name coordinates 
+
+=======
 - 
 ## Tutorial link : 
+>>>>>>> 7599a9e1e2fed26fe2ef861cd8efa88f42e906fd
 
-https://codeforgeek.com/2015/08/restful-api-node-mongodb/
 
 
 
--- GenerateAPI/Server.js
+++ GenerateAPI/Server.js
@@ -1,48 +1,112 @@
-var express     =   require("express");
-var app         =   express();
-var bodyParser  =   require("body-parser");
-var mongoOp     =   require("./models/mongo");
-var router      =   express.Router();
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoOp = require("./models/mongo");
+var router = express.Router();
+var https = require("https");
+var GeoJson = [];
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({"extended" : false}));
+app.use(bodyParser.urlencoded({
+    "extended": false
+}));
 
-router.get("/",function(req,res){
-    res.json({"error" : false,"message" : "Hello World"});
+router.get("/", function(req, res) {
+    res.json({
+        "error": false,
+        "message": "Hello World"
+    });
 });
+
 router.route("/data/:lat/:log")
-    .get(function(req,res){
-        mongoOp.find({},function(err,data){
-            if(err) {
-                res.send({"error" : true,"message" : "Error fetching data"});
-            } else {
+    .get(function(req, res) {
+        mongoOp.find({}, function(err, data) {
+            if (err) {
+                res.send({
+                    "error": true,
+                    "message": "Error fetching data"
+                });
+            }
+            else {
                 var front = '/maps/api/distancematrix/json?%20units=imperial%20&origins=';
-                var middle='&destinations=';
-                var end = '&mode=transit%20&transit_mode=bus%20&arrival_time=1461852459key=';
-                var key = 'AIzaSyAIE3Ly83t0yhWYQjrTWQFBX-Jq8umjfd4';
-                var fullpath = [];
-                var origions = req.params.lat+","+req.params.log;
-                    for(var i = 0; i < data.length; i++){
-                    var path = '';
-                                if (data[i].X===req.params.log && data[i].Y===req.params.lat ){
-                                continue;
+                var middle = '&destinations=';
+                var end = '&mode=transit%20&transit_mode=bus%20&arrival_time=1461852459&key=';
+                var key = 'AIzaSyCjaKNHLAHUjlnAziHgrRhqE3lYrvlgGMg';
+                var origions = req.params.lat + "," + req.params.log;
+                for (var i = 0; i < 5; i++) {
+                    var apipath = '';
+                    if (data[i].X === req.params.log && data[i].Y === req.params.lat) {
+                        continue;
+                    }
+                    if (i === 4) {
+                        res.send(GeoJson);
+                        GeoJson = [];
+                        break;
+                    }
+                    else {
+                        apipath = apipath + data[i].Y + "," + data[i].X;
+                        var api = front + origions + middle + apipath + end + key;
+
+                        var url = {
+                            host: 'maps.googleapis.com',
+                            path: api
+                        };
+
+                        var destipointLat = data[i].Y;
+                        var destipointLog = data[i].X;
+                        var index = data[i].id;
+
+                        var get_time = function(lat, logi, index, callback) {
+                            return https.get(url, function(response) {
+                                var result = '';
+                                response.on('error', function(err) {
+                                    console.log(err);
+                                });
+
+                                response.on('data', function(data) {
+                                    result += data;
+                                });
+
+                                response.on('end', function() {
+                                    var parsed = JSON.parse(result);
+                                    var log = {};
+                                    var time = parsed.rows[0].elements[0].duration.text;
+                                    log.time = time;
+                                    log.lat = lat;
+                                    log.logi = logi;
+                                    log.index = index;
+                                    callback(log);
+                                });
+                            })
+                        }
+
+                        var generateGeoJson = function(log) {
+                            GeoJson.push({
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'points',
+                                    coordinates: [log.logi, log.lat]
+                                },
+                                properties: {
+                                    Index: log.index,
+                                    From: log.index,
+                                    Time: log.time
                                 }
-                                if(i === data.length-1)
-                                {
-                                    break;
-                                }
-                                else{
-                                path = path+data[i].Y+","+data[i].X;
-                                fullpath.push({api:front+origions+middle+path+end+key,latitude:data[i].Y,logitude:data[i].X});
-                                continue;
-                                }
-                            }
-                res.send({fullpath});
+                            });
+                        }
+
+                        get_time(destipointLat, destipointLog, index, generateGeoJson);
+
+                        continue;
+
+                    }
+
+                }
             }
         });
     })
 
-app.use('/',router);
+app.use('/', router);
 
 app.listen(8080);
 console.log("Listening to PORT 8080");
